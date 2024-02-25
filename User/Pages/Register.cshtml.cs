e7fbc34ed5ecec9997b179400923330c9430a060
Hoang Tien <hoangtienbmt2911@gmail.com>
2024-02-26 01:47:11 +0700

    [TienPH] Front-end:  Login register for user, login for admin

---
+++ User/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace User.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
