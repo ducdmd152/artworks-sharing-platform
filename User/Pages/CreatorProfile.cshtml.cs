dfb72f8134d6eabef3ea707f3485a459795871e2
Hoang Tien <hoangtienbmt2911@gmail.com>
2024-03-02 04:46:27 +0700

    [TienPH] FE: Done my profile creator, my artwork list view, my artworks detail view

---
+++ User/Pages/CreatorProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace User.Pages
+{
+    public class CreatorProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
