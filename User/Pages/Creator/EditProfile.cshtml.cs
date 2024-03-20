11bf68426c3b14348d243d1c17a122dca3d81f7a
Hoang Tien <hoangtienbmt2911@gmail.com>
2024-03-20 21:03:13 +0700

    [TienPH] Update profile creator, FE edit profile

---
+++ User/Pages/Creator/EditProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace User.Pages.Creator
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
