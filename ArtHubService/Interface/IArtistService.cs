35e0132ed98d43fd5901bc6780434a3cad7434b2
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-03-25 18:56:42 +0700

    Audience | Load Creator Profile

--- ArtHubService/Interface/IArtistService.cs
+++ ArtHubService/Interface/IArtistService.cs
@@ -7,4 +7,5 @@ public interface IArtistService
 {
     Task<PageResult<ArtistDataDto>> GetArtistInforSummaryByCondition(SearchPayload<ArtistSearchConditionDto> searchPayload);
     PageResult<SelectCreatorDTO> GetTopCreators(string audienceEmail, int pageIndex, int pageSize);
+    SelectCreatorDTO GetCreatorByEmail(string creatorEmail, string audienceEmail);
 }
