d4c809a110356694dd8b1f6dd59067413d0cff7f
Hoang Tien <hoangtienbmt2911@gmail.com>
2024-03-26 20:03:31 +0700

    [TienPH] Send mail sample

--- User/Pages/Authenticate/ForgotPassword.cshtml.cs
+++ User/Pages/Authenticate/ForgotPassword.cshtml.cs
@@ -1,3 +1,6 @@
+using ArtHubBO.DTO;
+using ArtHubService.Interface;
+using ArtHubService.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +8,21 @@ namespace User.Pages.Authenticate
 {
     public class ForgotPasswordModel : PageModel
     {
+        private readonly IEmailService emailService;
+
+        public ForgotPasswordModel(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
         public void OnPost()
         {
-
+            emailService.SendEmail(new SendEmailDto
+            {
+                Subject = "Huhu",
+                ToEmail = "hoangtienbmt2911@gmail.com",
+                Body = "<html><body> hehe </body></html>"
+            });
         }
     }
 }
